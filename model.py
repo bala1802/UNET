c70960cadee7ebfff674541395619af943d5cf7d
bala1802 <bala1802@live.com>
2023-10-03 22:45:08 +0530

    forward layer is in progress for the model

--- model.py
+++ model.py
@@ -55,6 +55,17 @@ class UNET(nn.Module):
         #features[-1] because the end layer 512 in converted to 1024
         self.bottleneck = DoubleConv(in_channels=features[-1], out_channels=features[-1]*2)
         self.final_conv = nn.Conv2d(in_channels=features[-1], out_channels=out_channels, kernel_size=1)
+    
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+        
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
 
         
 
